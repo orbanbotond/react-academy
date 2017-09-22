bf1180a397d022268d78625ac6e77df0153cf629
Orban Botond <orbanbotond@gmail.com>
2017-09-22 19:39:56 +0300

    The user is there.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+boti = User.create name: 'Boti', admin: true, password: 'sex'
+gyongyi = User.create name: 'Gyongyi', admin: false, password: 'sex'
