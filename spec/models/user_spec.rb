41525a246f4a5f940a506d5f8cdab362d5798a87
Orban Botond <orbanbotond@gmail.com>
2017-09-22 18:43:11 +0300

    User can be authenticated.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe User do
+  context 'fields' do
+    it { should respond_to(:name) }
+    it { should respond_to(:password_hash) }
+  end
+
+  context '#' do
+    specify 'The password should be stored hashed' do
+      user = described_class.new
+      unhashed_pwd = 'New Pwd'
+      user.password = unhashed_pwd
+      expect(user.password_hash).to_not eq(unhashed_pwd)
+      expect(user.password).to eq(unhashed_pwd)
+    end
+  end
+end
